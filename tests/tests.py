7ea4df1ff721b58b5aa5353ede73c78eddbfcc58
mv_taliani <mv.taliani@gmail.com>
2023-12-19 00:39:07 -0300

    Update testes para lidar com erros e situações comuns

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,11 @@
-import unittest
+import io
+import logging
 
 import unittest
 import tempfile
 import os
+from tkinter import Tk
+from unittest.mock import patch
 
 # Importe a função que você deseja testar
 from delphia_csv.app.app import processar_transacoes
@@ -13,6 +16,11 @@ class TestProcessarTransacoes(unittest.TestCase):
     def setUp(self):
         # Cria um arquivo temporário de entrada para os testes
         self.arquivo_entrada = tempfile.NamedTemporaryFile(delete=False)
+        # Cria um arquivo temporário de saída para os testes
+        self.arquivo_saida = tempfile.NamedTemporaryFile(delete=False)
+
+        self.arquivo_vazio = tempfile.NamedTemporaryFile(delete=False)
+
         with open(self.arquivo_entrada.name, 'w', newline='') as file:
             file.write("Nome,Valor,Data\n")
             file.write("Cliente1,1500,2023-01-01\n")
@@ -20,31 +28,42 @@ class TestProcessarTransacoes(unittest.TestCase):
             file.write("Cliente3,1200,2023-01-03\n")
 
     def tearDown(self):
-        # Remove o arquivo temporário de entrada após os testes
+        # Remove os arquivo temporários após os testes
         self.arquivo_entrada.close()
         os.remove(self.arquivo_entrada.name)
+        self.arquivo_saida.close()
+        os.remove(self.arquivo_saida.name)
 
     def test_processar_transacoes(self):
-        # Cria um arquivo temporário de saída para os testes
-        arquivo_saida = tempfile.NamedTemporaryFile(delete=False)
+        processar_transacoes(self.arquivo_entrada.name, self.arquivo_saida.name, 1000)
 
-        try:
-            # Chama a função que você deseja testar
-            processar_transacoes(self.arquivo_entrada.name, arquivo_saida.name, 1000)
+        # Lê o conteúdo do arquivo de saída gerado pela função
+        with open(self.arquivo_saida.name, 'r', newline='') as file:
+            linhas = file.readlines()
 
-            # Lê o conteúdo do arquivo de saída gerado pela função
-            with open(arquivo_saida.name, 'r', newline='') as file:
-                linhas = file.readlines()
+        # Verifica se a função gerou o resultado esperado
+        self.assertEqual(len(linhas), 3)  # Incluindo o cabeçalho, devem haver três linhas
+        self.assertIn("Cliente1,1500.0,2023-01-01\r\n", linhas)  # Transação acima de $1000
+        self.assertIn("Cliente3,1200.0,2023-01-03\r\n", linhas)  # Transação acima de $1000
 
-            # Verifica se a função gerou o resultado esperado
-            self.assertEqual(len(linhas), 3)  # Incluindo o cabeçalho, deve haver três linhas
-            self.assertIn("Cliente1,1500.0,2023-01-01\r\n", linhas)  # Transação acima de $1000
-            self.assertIn("Cliente3,1200.0,2023-01-03\r\n", linhas)  # Transação acima de $1000
+    def test_processamento_com_sucesso_sem_transacoes_acima_do_valor(self):
+        sucesso, mensagem_erro = processar_transacoes(self.arquivo_vazio.name, self.arquivo_saida.name, 5000)
 
-        finally:
-            # Remove o arquivo temporário de saída após os testes
-            arquivo_saida.close()
-            os.remove(arquivo_saida.name)
+        self.assertTrue(sucesso)
+        self.assertEqual('', mensagem_erro)
+
+    def test_processamento_com_erro_csv(self):
+        sucesso, mensagem_erro = processar_transacoes('arquivo_invalido.csv', self.arquivo_saida.name, 1000)
+
+        self.assertFalse(sucesso)
+        self.assertIn("Arquivo não encontrado: arquivo_invalido.csv", mensagem_erro)
+
+    def test_processamento_com_erro_valor(self):
+        sucesso, mensagem_erro = processar_transacoes(self.arquivo_entrada.name, self.arquivo_saida.name,
+                                                      'valor_invalido')
+
+        self.assertFalse(sucesso)
+        self.assertIn('Valor mínimo inválido', mensagem_erro)
 
 
 if __name__ == '__main__':
